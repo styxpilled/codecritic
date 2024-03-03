9d7815a3395e9ac8a91d4b44d2df85237af39e0b
styxpilled <styxpilled.dev@gmail.com>
2024-03-03 04:56:03 +0100

    feat: fetch package readme from github

--- src/routes/packages/[package]/+page.svelte
+++ src/routes/packages/[package]/+page.svelte
@@ -5,6 +5,7 @@
 	import { marked } from 'marked';
 	import RatingInput from '$ui/RatingInput.svelte';
 	import Review from '$ui/Review.svelte';
+	import List from '$ui/List.svelte';
 	export let data;
 
 	onMount(() => {
@@ -27,11 +28,13 @@
 					<li>{keyword}</li>
 				{/each}
 			</ul>
-			{#if data.package.readme}
-				{#await marked.parse(data.package.readme, { gfm: true }) then readme}
-					{@html readme}
-				{/await}
-			{/if}
+			<div class="readme">
+				{#if data.readme}
+					{#await marked.parse(data.readme, { gfm: true }) then readme}
+						{@html readme}
+					{/await}
+				{/if}
+			</div>
 		{/if}
 	</div>
 	<div class="package-sidebar">
@@ -39,11 +42,11 @@
 			<p>Install</p>
 			<p class="command">pnpm add {data.package.name}</p>
 		</div>
+		{data.package.repository.url}
 		{#if data.package.repository && data.package.repository.type === 'git'}
-			{@const repository = data.package.repository.url.substring(
-				4,
-				data.package.repository.url.length - 4
-			)}
+			{@const url = data.package.repository.url}
+			{@const repository =
+				'https://github.com' + url.substring(url.indexOf('github.com') + 10, url.length - 4)}
 			<div>
 				<p>Repository</p>
 				<a href={repository}>{repository}</a>
@@ -68,17 +71,30 @@
 			</label>
 			<button type="submit">Save</button>
 		</form>
+		<div class="reviews">
+			{#each data.reviews as review}
+				<Review {review} />
+			{/each}
+		</div>
 	</div>
-</div>
-<div class="reviews">
-	{#each data.reviews as review}
-		<Review {review} />
-	{/each}
 </div>
 
 <style>
 	.package {
-		display: flex;
+		display: grid;
+		grid-template-columns: 3fr 2fr;
+		grid-template-rows: 1fr;
+		gap: 0px 0px;
+		grid-template-areas: 'body sidebar';
+	}
+
+	.package-body {
+		grid-area: body;
+		padding: 0.5rem;
+	}
+
+	.package-sidebar {
+		grid-area: sidebar;
 	}
 
 	form {
@@ -87,6 +103,11 @@
 		background-color: #383d45;
 		padding: 1rem;
 		gap: 1rem;
+	}
+
+	textarea {
+		resize: none;
+		height: 6rem;
 	}
 
 	.keywords {
@@ -100,8 +121,16 @@
 		border-radius: 0.25rem;
 	}
 
-	:global(.package img) {
+	:global(.readme img) {
 		display: inline-block;
+		/* display: flex; */
+	}
+
+	:global(.readme > h1),
+	:global(.readme > h2),
+	:global(.readme > h3),
+	:global(.readme > p) {
+		margin: 1rem 0rem;
 	}
 	/* :global(.package p) {
 		display: flex;
--- src/routes/packages/[package]/+page.ts
+++ src/routes/packages/[package]/+page.ts
@@ -5,6 +5,24 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ fetch, params }) => {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const pkg: any = await fetchOr(`https://registry.npmjs.org/${params.package}`, undefined, fetch);
-	const reviews = await fetchOr<Review[]>(`/api/packages/${params.package}/review`, [], fetch);
-	return { package: pkg, reviews };
+	let readme = null;
+
+	if (pkg.repository) {
+		const repo: string = pkg.repository.url;
+		const readmeData = await fetchOr<{ content: string }>(
+			`https://api.github.com/repos/${repo.substring(
+				repo.indexOf('github.com') + 11,
+				repo.length - 4
+			)}/contents/README.md`,
+			undefined,
+			fetch
+		);
+		if (readmeData !== undefined && readmeData.content) {
+			readme = atob(readmeData.content);
+		}
+	}
+
+	// const repo = pkg.repository;
+	const reviews = fetchOr<Review[]>(`/api/packages/${params.package}/review`, [], fetch);
+	return { package: pkg, readme, reviews: await reviews };
 };
