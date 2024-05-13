0eb005b722a159bbe0d809701ebcb0de6bb43a8a
styxpilled <styxpilled.dev@gmail.com>
2024-05-13 18:20:05 +0200

    chore: remove old code

--- src/routes/api/profile/[id]/reviews/+server.ts
+++
@@ -1,7 +0,0 @@
-import { ok } from '$lib/server/index.ts';
-
-export const GET = async ({ locals: { supabase }, params }) => {
-	const { data: reviews } = await supabase.from('reviews').select('*').eq('author', params.id);
-
-	return ok(reviews);
-};
